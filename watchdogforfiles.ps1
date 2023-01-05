85e18a54b7323d0e0acf42b5001151812d4e48f4
Sparks <34066194+SparksSkywere@users.noreply.github.com>
2023-01-05 10:28:09 +0000

    v0.2
    Auto restart update, plus many changes to the code to implement the auto restart. I added a parameter for the restart functions

--- watchdogforfiles.ps1
+++ watchdogforfiles.ps1
@@ -6,24 +6,41 @@ param (
 	#The interval, in seconds, at which to check the status of the process
 	[Parameter(Mandatory=$true)][Uint32]$interval,
 	#The email address to send notifications to
-	[Parameter(Mandatory=$true)][string]$notificationEmail
+	[Parameter(Mandatory=$true)][string]$notificationEmail,
+	#Enable Auto restart when the application fails
+	[Parameter(Mandatory=$false)][Switch]$Autorestart = $false
 )
-
 #Function to check the status of the process
 function checkstatus {
-	# Check if the process is running
-	$process = Get-Process -Name $processName
-	if ($null -eq $process ) {
-		# If the process is not running, send an email notification and exit the script
-		Write-Output "$processName is not running, sending email"
-		send-email -subject "Process Not Running" -body "$processName is not running on the server. Please investigate." -to $notificationEmail
-		exit
+#Get process that is running, just the process name, no extensions used
+$process = Get-Process -Name $processName
+	#Check status of the program, along with auto restart
+	if ($Autorestart -eq $true){
+		if ($null -eq $process ) {
+			#If the process is not running, send an email notification and exit the script with auto restart enabled
+			Write-Output "$processName is not running, sending notification and restarting application"
+			send-email -subject "Watchdog Notification" -body "$processName has stopped working, Restarting application." -to $notificationEmail
+			Start-Process $ProcessPath.FileName
+		}
+		else {
+			Write-Output "Auto restart enabled"
+			Write-Output "$processName is running"
+		}
 	}
-	else {
-		Write-Output "$processName is running"
+	#Check status of the program - Listen only
+	if ($Autorestart -eq $false) {
+		if ($null -eq $process ) {
+			#If the process is not running, send an email notification and exit the script with auto restart disabled
+			Write-Output "$processName is not running, sending email"
+			send-email -subject "Watchdog Notification" -body "$processName has stopped working. Please investigate." -to $notificationEmail
+		exit
+		}
+		else {
+			Write-Output "Auto restart not enabled"
+			Write-Output "$processName is running"
+		}
 	}
 }
-
 #Function to send an email notification
 function send-email {
 	param (
@@ -41,15 +58,17 @@ function send-email {
 	$mailMessage.To.Add($to)
 	$mailMessage.Subject = $subject
 	$mailMessage.Body = $body
-	
+
 	#Set up the SMTP client
 	$smtpClient = New-Object System.Net.Mail.SmtpClient
 	$smtpClient.Host = "smtp.email.com"
 	$smtpClient.Credentials = New-Object System.Net.NetworkCredential("username", "password")
-	
+
 	#Send the email
 	$smtpClient.Send($mailMessage)
 }
+#Get process path, this value is saved in memory and will only be used for restarting the application
+$ProcessPath = Get-Process $processName -FileVersionInfo | Select-Object FileName
 
 #Run the check-status function at the specified interval
 while ($true) {
